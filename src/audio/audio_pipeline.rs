771df0c024be0d52778084803eb0961f86150359
ritvars <ritvarszvejnieks@protonmail.com>
2026-02-03 21:26:29 +0200

    refactor: use watch in favour of mutex to track recording state.

--- src/audio/audio_pipeline.rs
+++ src/audio/audio_pipeline.rs
@@ -1,13 +1,11 @@
-use std::sync::Arc;
-
 use cpal::traits::StreamTrait;
-use tokio::{sync::{Mutex, mpsc::{self, Receiver}}, task::JoinHandle};
+use tokio::{sync::{mpsc::{self, Receiver}}, task::JoinHandle};
 
 use crate::{RECORDING_TIMEOUT_SECS, audio::{self, speech_to_text::SttMessage}};
 
 pub struct AudioPipeline {
     stream: cpal::Stream,
-    is_recording: Arc<Mutex<bool>>,
+    is_recording_tx: tokio::sync::watch::Sender<bool>,
     resampler_handle: JoinHandle<()>,
     stt_handle: JoinHandle<()>,
     capture_handle: JoinHandle<()>,
@@ -15,7 +13,6 @@ pub struct AudioPipeline {
 
 impl AudioPipeline {
     pub async fn new() -> Result<(Self, Receiver<SttMessage>), Box<dyn std::error::Error>> {
-        let is_recording = Arc::new(Mutex::new(false));
         let (stream, stream_config, recorder_rx) = audio::recorder::init()?;
         let (resampled_rx, resampler_handle) = audio::resampler::init(
             recorder_rx,
@@ -23,15 +20,20 @@ impl AudioPipeline {
             stream_config.input_channels,
         ).await?;
         let (stt_tx, audio_buffer_rx) = mpsc::channel::<Vec<f32>>(1);
-        let capture_handle = init_audio_capture(resampled_rx, stt_tx, is_recording.clone()).await?;
+        let (is_recording_tx, is_recording_rx) = tokio::sync::watch::channel(false);
+        let capture_handle = init_audio_capture(
+            resampled_rx,
+            stt_tx,
+            is_recording_rx
+        ).await?;
         let (stt_rx, stt_handle) = audio::speech_to_text::init(audio_buffer_rx).await?;
 
         let pipeline = AudioPipeline {
+            is_recording_tx,
             stream,
             resampler_handle,
             stt_handle,
             capture_handle,
-            is_recording,
         };
 
         Ok((pipeline, stt_rx))
@@ -55,46 +57,48 @@ impl AudioPipeline {
     /// If buffer reaches timeout size, stop recording and transcribe.
     pub async fn start_recording(&self) {
         self.play();
-        *self.is_recording.lock().await = true;
+        self.is_recording_tx.send(true).expect("Failed to send recording state");
     }
 
     /// Stop stream, send accumulated audio_buffer to STT, and clear buffer.
     pub async fn stop_recording_and_transcribe(&self) {
         self.pause();
-        *self.is_recording.lock().await = false;
+        self.is_recording_tx.send(false).expect("Failed to send recording state");
     }
 }
 
 async fn init_audio_capture(
     mut rx: mpsc::Receiver<Vec<f32>>,
     tx: mpsc::Sender<Vec<f32>>,
-    is_recording: Arc<Mutex<bool>>
+    mut is_recording_rx: tokio::sync::watch::Receiver<bool>,
 ) -> Result<JoinHandle<()>, Box<dyn std::error::Error>> {
     let handle = tokio::spawn(async move {
         let mut buffer = Vec::<f32>::with_capacity(16_000 * RECORDING_TIMEOUT_SECS as usize);
 
         loop {
-            // todo: this is dumb?
-            // todo2: this guy probably dies after exceeding buffer once. (or mutex deadlock?)
-            match *is_recording.lock().await {
-                true => {
-                    while let Ok(data) = rx.try_recv() {
-                        buffer.extend_from_slice(&data);
-                        // If buffer length exceeds timeout, stop recording and send to STT
-                        if buffer.len() >= 16_000 * RECORDING_TIMEOUT_SECS as usize {
-                            tx.send(buffer.clone()).await.unwrap();
-                            buffer.clear();
-                            *is_recording.lock().await = false;
+            tokio::select! {
+                _ = is_recording_rx.changed() => {
+                    if !*is_recording_rx.borrow() && !buffer.is_empty() {
+                        if tx.send(buffer.clone()).await.is_err() {
+                            break;
                         }
-                    }
-                },
-                false => {
-                    if !buffer.is_empty() {
-                        tx.send(buffer.clone()).await.unwrap();
                         buffer.clear();
                     }
-                },
-            };
+                }
+
+                Some(data) = rx.recv() => {
+                    if *is_recording_rx.borrow() {
+                        buffer.extend_from_slice(&data);
+                        // todo: figure out how to stop recording.
+                        // if buffer.len() >= 16_000 * RECORDING_TIMEOUT_SECS as usize {
+                        //     if tx.send(buffer.clone()).await.is_err() {
+                        //         break;
+                        //     }
+                        //     buffer.clear();
+                        // }
+                    }
+                }
+            }
         }
     });
 
